15ad21a8039890f5a843f7fa37304b9d94250892
Ryan Garrity <rgarrity7000@gmail.com>
2022-08-27 17:41:42 -0400

    Add project files.

---
+++ BasicNeuralNetTesting/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NNetTesting {
+    public class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World");
+
+            Network network = new Network();
+
+        }
+    }
+}
